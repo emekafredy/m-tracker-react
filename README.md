84e5d9d389d852041bd3e4b93be15161329be004
emekafredy <chinedufrderick@gmail.com>
2018-10-30 18:36:42 +0100

    ft(updateRequest) users can update their requests
    - set up redux store for update
    - implement feature to update request
    
    [Delivers #161585216]

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # m-tracker-react
 
+- Maintenance Tracker App, built with ReactJS
+
 [![CircleCI](https://circleci.com/gh/emekafredy/m-tracker-react.svg?style=svg)](https://circleci.com/gh/emekafredy/m-tracker-react)
+
+
+# Users
+
+- Sign up page for new users and Login page for registered users
+- Pages for users to create, edit or cancel requests
+- User can also see all requests made
+
+
+# Admin
+
+- Logged in admin can see every employees' requests
+- Admin can process employees' requests by either approving or rejecting them
+- When request has been resolved, admin can mark as resolved
+
+
+# App hosted on heroku
+- https://emeka-m-tracker-react.herokuapp.com/
+
+
+# Project Management Tool
+
+- https://www.pivotaltracker.com/n/projects/2198023
---
+++ src/components/ProtectRoutes/PrivateRoute.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+class PrivateRoute extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
+  render() {
+    const { component: ProtectedComponent, auth: { isAuthenticated }, ...rest } = this.props
+    return (
+      <Route
+        {...rest}
+
+        render={props => (
+          isAuthenticated
+            ? <ProtectedComponent {...props} />
+            : (
+              <Redirect to={{
+                pathname: '/login',
+                state: { from: props.location }
+              }}
+              />
+            )
+        )}
+      />
+    );
+  }
+}
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, null, null, { pure: false })(PrivateRoute);
