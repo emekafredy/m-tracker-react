e5891721ce3c8519c792a78f09b16485b27bae4f
emekafredy <chinedufrderick@gmail.com>
2018-10-30 13:30:29 +0100

    ft(processReqeusts) admin can process requests
    - Admin can either approve or disapprove a pending request
    - Admin can resolve an approved request
    
    [Delivers #161573172]

---
+++ src/actions/processRequest.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+// action types
+import { PROCESS_REQUEST, PROCESS_REQUEST_SUCCESS } from '../actionTypes/processRequest.types';
+import { GET_ERRORS } from '../actionTypes/errors.types';
+
+const API = 'https://emeka-m-tracker.herokuapp.com';
+
+const processRequestLoading = () => {
+  return {
+    type: PROCESS_REQUEST,
+  }
+}
+
+// process request
+export const processRequest = (requestId, status) => dispatch => {
+  const { jwtToken } = localStorage;
+  dispatch(processRequestLoading());
+  axios
+    .put(`${API}/api/v1/requests/${requestId}/${status}`, {},
+      {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    .then(response => {
+      dispatch({
+        type: PROCESS_REQUEST_SUCCESS,
+        payload: response.data
+      })
+    })
+    .catch(error => dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    }));
+};
